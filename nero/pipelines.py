74c796ec661a142453c9ec08e2027ce951b7d566
Cloudy Young <cloudy.young@outlook.com>
2021-10-16 03:54:20 -0600

    Add CourseRequisitesPipeline

--- nero/pipelines.py
+++ nero/pipelines.py
@@ -9,6 +9,8 @@
 import json
 import re
 from itemadapter import ItemAdapter
+from bs4 import BeautifulSoup
+from nero.items import CourseInfo, CourseRequisite
 
 
 class FileStorePipeline:
@@ -34,3 +36,167 @@ class FileStorePipeline:
             self.files[item_type].write(line)
 
         return item
+
+
+class CourseRequisitesPipeline:
+    # CourseTitle, for replacing full course names with short course codes
+    course_titles = {}
+
+    def __init__(self, spider):
+        title_year_file = open("data/course-title-year.jsonlines", "r")
+        for line in title_year_file:
+            course_title = json.loads(line)
+            title = course_title["title"]
+            code = course_title["code"]
+            self.course_titles[title] = code
+        title_year_file.close()
+        print(self.course_titles)
+
+    def prereq_single_course(self, string):
+        return string
+
+    def prereq_x_units_from(self, string):
+        m = re.match(self.regex_x_units_from, string)
+        units_text = m.group(0)
+        courses_text = m.group(1)
+
+        courses = []
+        last_code = ""
+
+        _courses = re.split(',|or', courses_text)
+        for _course in _courses:
+            x = re.match(self.regex_complete_course, _course)
+            y = re.match(self.regex_incomplete_course, _course)
+
+            code, key, course_title = "", "", ""
+
+            # If the course name is complete `Arts 241`, or incomplete `241`
+            if(x):
+                code = last_code = x.group(1)
+                key = x.group(2)
+            elif(y):
+                code = last_code
+                key = y.group(1)
+
+            # Trim spaces
+            code, key, course_title = code.strip(), key.strip(), course_title.strip()
+
+            course_title = code + " " + key
+            if(course_title == _courses):
+                courses.append(course_title)
+            else:
+                courses.append(_course)  # Has to deal with this new pattern
+                print("UNHANDLED PATTERN: " + _course)
+
+        if(courses == []):
+            print("UNHANDLED PATTERN: " + string)
+            return string
+
+        return {"units": int(units_text), "courses": courses}
+
+    def prereq_consent_of_the(self, string):
+        m = re.match(self.regex_consent_of_the, string)
+
+        if(m):
+            return {"consent": m.group(1).strip()}
+        else:
+            print("UNHANDLED PATTERN: " + string)
+            return string
+
+    def prereq_or(self, string):
+        m = re.match(self.regex_or, string)
+        if(m):
+            code1, number1, code2, number2 = m.group(1), m.group(2), None, None
+
+            code_or_number = m.group(3)
+            if(re.match(self.regex_incomplete_course, code_or_number)):
+                # This is a number
+                code2 = code1
+                number2 = m.group(3)
+            else:
+                code2 = m.group(3)
+                number2 = m.group(4)
+
+            # Trim spaces
+            code1, number1, code2, number2 = code1.strip(), number1.strip(), code2.strip(), number2.strip()
+            return [code1 + " " + number1, code2 + " " + number2]
+        else:
+            print("UNHANDLED PATTERN: " + string)
+            return string
+
+    # Regex string
+    regex_incomplete_course = "([0-9]{3})"
+    regex_complete_course = "([A-z ]*) ([0-9]{3})"
+    regex_x_units_from = "([0-9]) units from (.*)"
+    regex_consent_of_the = "Consent of the ([A-z ]*)"
+    regex_or = "([A-z ]*) ([0-9]{3}) or ([A-z ]*)?([0-9]{3})"
+    regex_single_course = "([A-z ]*) ([0-9]{3})$"
+
+    # All regex and its matching handle function
+    # Tests on regex101.com
+    patterns = {
+        regex_x_units_from: prereq_x_units_from,
+        regex_consent_of_the: prereq_consent_of_the,
+        regex_or: prereq_or,
+        regex_single_course: prereq_single_course,
+    }
+
+    def open_spider(self, spider):
+        self.file = open("data/course-requisites.jsonlines", 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        if(isinstance(item, CourseInfo)):
+            course_requisite_obj = CourseRequisite(cid=item["cid"],
+                                                   code=item["code"],
+                                                   number=item["number"],
+                                                   prereq=self.prereq(item['prereq']))
+
+            line = json.dumps(ItemAdapter(course_requisite_obj).asdict()) + "\n"
+            self.file.write(line)
+
+        return item
+
+    def prereq(self, prereq_text):
+        # If no prereq, return nothing
+        if(prereq_text == None):
+            return None
+
+        # Remove all the html tags in requisites
+        soup = BeautifulSoup(str(prereq_text), 'html.parser')
+        prereq_text = str(soup.get_text())
+
+        # Trim trailing period
+        prereq_text = prereq_text.rstrip('.')
+
+        # Replace full course names with short course codes
+        full_names = re.findall("([A-Z][A-z ]*) ([0-9]{3})", prereq_text)
+        for (full_name, _) in full_names:
+            full_name = full_name.strip()
+            if full_name in self.course_titles:
+                prereq_text = re.sub(full_name, self.course_titles[full_name], prereq_text)
+            else:
+                print("Titles error, does not exist: " + full_name)
+
+        prereq = [prereq_text]
+
+        # _ands = _prereq.split(";")
+        # if(len(_ands) == 1):  # If the requisite is not combined `x;y;z` but `x and y`.
+        #     _ands = _prereq.split("and")
+
+        # # For each and condition
+        # for _and in _ands:
+        #     _and = str(_and).strip()
+
+        #     # Match for a specific pattern for this and condition
+        #     for regex in self.patterns.keys():
+
+        #         # If found, pass in the and conditition to associated regex function
+        #         if (re.search(regex, _and)):
+        #             prereq.append(self.patterns[regex](self, _and))
+        #             break
+
+        # print(_prereq, prereq)
+        return prereq
--- nero/settings.py
+++ nero/settings.py
@@ -37,10 +37,10 @@ DOWNLOAD_DELAY = 1.2
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
@@ -51,19 +51,20 @@ SPIDER_MIDDLEWARES = {
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-   'nero.middlewares.NeroDownloaderMiddleware': 543,
+    'nero.middlewares.NeroDownloaderMiddleware': 543,
 }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'nero.pipelines.FileStorePipeline': 543,
+    'nero.pipelines.CourseRequisitesPipeline': 544,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
