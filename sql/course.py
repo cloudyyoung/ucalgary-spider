731089223d131ca6ff6b985d4e9c5c0a29a5dfb8
Cloudy Young <cloudy.young@outlook.com>
2021-12-17 19:56:52 -0700

    Rename

--- sql/course.py
+++ sql/course.py
@@ -3,7 +3,7 @@ import json
 
 # Open data/course-info.jsonline, read each line and parse into json
 f = open("data/course-info.jsonlines", "r")
-w = open("sql/course-info.json", "w")
+w = open("sql/course-info.sql", "w")
 for line in f:
     # Parse to json
     json_data = json.loads(line)
@@ -24,7 +24,8 @@ for line in f:
         else:
             json_data[key] = "'" + str(json_data[key]) + "'"
 
-    sql = "INSERT INTO `course` (`course_id`, `code`, `number`, `topic`, `description`, `credits`, `units`, `prerequisites`, `antirequisites`, `corequisites`, `no_gpa`, `repeat`, `notes`) "
+    # sql = "INSERT INTO `course` (`course_id`, `code`, `number`, `topic`, `description`, `credits`, `units`, `prerequisites`, `antirequisites`, `corequisites`, `no_gpa`, `repeat`, `notes`) "
+    sql = "INSERT INTO `course` "
     sql += "VALUES (" + str(json_data["cid"]) + ", " + str(json_data["code"]) + ", " + str(json_data["number"]) + ", " + str(json_data["topic"]) + ", " + str(json_data["description"]) + ", " + str(json_data["credits"]) + ", " + str(json_data["units"]) + ", " + str(json_data["prereq"]) + ", " + str(json_data["antireq"]) + ", " + str(json_data["coreq"]) + ", " + str(json_data["nogpa"]) + ", " + str(json_data["repeat"]) + ", " + str(json_data["notes"]) + ");"
     
     # Write to file
