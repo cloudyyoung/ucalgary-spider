4424b1de818169d8870ab6417185fd0e896df2c5
Cloudy Young <cloudy.young@outlook.com>
2021-12-17 21:20:31 -0700

    Update

--- concentrate.sql
+++ concentrate.sql
@@ -1,9 +1,9 @@
-INSERT INTO `concentration` VALUES ('Concentration in Computer Game Development');
-INSERT INTO `concentration` VALUES ('Concentration in Software Engineering');
-INSERT INTO `concentration` VALUES ('Concentration in Theoretical Computer Science');
-INSERT INTO `concentration` VALUES ('Concentration in Scientific Computation');
-INSERT INTO `concentration` VALUES ('Concentration in Human-Computer Interactions');
-INSERT INTO `concentration` VALUES ('Concentration in Computer Graphics');
-INSERT INTO `concentration` VALUES ('Concentration in Information Security');
-INSERT INTO `concentration` VALUES ('Concentration in Networks and Distributed Computing');
-INSERT INTO `concentration` VALUES ('Concentration in Visualization and Analytics');
+INSERT INTO `concentration` VALUES(94301, 'Concentration in Computer Game Development', '');
+INSERT INTO `concentration` VALUES(94301, 'Concentration in Software Engineering', '');
+INSERT INTO `concentration` VALUES(94301, 'Concentration in Theoretical Computer Science', '');
+INSERT INTO `concentration` VALUES(94301, 'Concentration in Scientific Computation', '');
+INSERT INTO `concentration` VALUES(94301, 'Concentration in Human-Computer Interactions', '');
+INSERT INTO `concentration` VALUES(94301, 'Concentration in Computer Graphics', '');
+INSERT INTO `concentration` VALUES(94301, 'Concentration in Information Security', '');
+INSERT INTO `concentration` VALUES(94301, 'Concentration in Networks and Distributed Computing', '');
+INSERT INTO `concentration` VALUES(94301, 'Concentration in Visualization and Analytics', '');
