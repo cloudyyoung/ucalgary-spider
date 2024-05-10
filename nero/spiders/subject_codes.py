fa4fa57305a4ea2ec4a0f5b06325707c6fb51bdb
Yunfan Yang <cloudy.young@outlook.com>
2024-05-09 23:14:31 -0600

    Change subject code crawling approach

--- nero/spiders/subject_codes.py
+++ nero/spiders/subject_codes.py
@@ -1,4 +1,5 @@
 import json
+import re
 from scrapy import Spider, Request
 from nero.items import SubjectCode
 
@@ -8,11 +9,20 @@ class SubjectCodeSpider(Spider):
 
     def start_requests(self):
         url = "https://app.coursedog.com/api/v1/ca/ucalgary_peoplesoft/search-configurations/3HheDcKChSNwS1Wr1Khr"
-        print(url)
+        url_extra = "https://calendar.ucalgary.ca/courses?page=1&cq="
 
+        # print(url)
+        # yield Request(
+        #     url=url,
+        #     callback=self.parse,
+        #     method="GET",
+        #     headers={"Content-Type": "application/json"},
+        # )
+
+        print(url_extra)
         yield Request(
-            url=url,
-            callback=self.parse,
+            url=url_extra,
+            callback=self.parse_extra,
             method="GET",
             headers={"Content-Type": "application/json"},
         )
@@ -29,6 +39,32 @@ class SubjectCodeSpider(Spider):
             value = str(option["value"])
             label = str(option["label"])
             code, title = label.split(" - ", 1)
+
+            yield SubjectCode(
+                code=code,
+                title=title,
+            )
+
+    def parse_extra(self, response):
+        body = str(response.body, encoding="utf-8")
+        start_string = 'Subject","Derived field from Subject code and Manual Input of Subject name",'
+        end_string = "));"
+
+        start = body.find(start_string)
+        end = body.find(end_string, start)
+
+        content = body[start + len(start_string) : end]
+
+        # Find all strings within double quotes and decode unicode escapes
+        strings = re.findall(r'"([^"]*)"', content)
+        strings = [s.encode().decode("unicode_escape") for s in strings]
+
+        # Filter ones that has <p> tags
+        strings = [s for s in strings if "<p>" in s]
+
+        for string in strings:
+            code, title = re.findall(r"^<p>([A-Z]+) - (.+)</p>", string)[0]
+            print(code, title)
 
             yield SubjectCode(
                 code=code,
