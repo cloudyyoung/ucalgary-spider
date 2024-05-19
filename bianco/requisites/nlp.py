dc95e38e41fb696140f82169ccc0a5395f943bd7
Yunfan Yang <cloudy.young@outlook.com>
2024-05-19 02:12:34 -0600

    Update nlp.py

--- bianco/requisites/nlp.py
+++ bianco/requisites/nlp.py
@@ -5,7 +5,7 @@ from requisites.pipes.fix_ent_head import *
 from requisites.pipes.detect_entity import *
 from requisites.pipes.merge_entity_spans import *
 from requisites.pipes.expand_course_code import *
-from bianco.requisites.pipes.default_pipeline import *
+from requisites.pipes.default_pipeline import *
 
 nlp = spacy.load("en_core_web_sm", exclude=["ner"])
 nlp.add_pipe("merge_complex_course_number")
