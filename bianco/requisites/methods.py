20e80bf383877934fae46a5270a158b41954372e
Yunfan Yang <cloudy.young@outlook.com>
2024-05-19 01:04:10 -0600

    Disable things

--- bianco/requisites/methods.py
+++ bianco/requisites/methods.py
@@ -1,22 +1,16 @@
 from spacy import displacy
 
-from requisites.expand_nlp import expand_nlp
-from requisites.constituency_nlp import constituency_nlp
-from requisites.structure_nlp import structure_nlp
+from requisites.nlp import nlp
 from requisites.utils import extract_doc, replace_subject_code
 
 
 def try_nlp(course: dict, sent: str):
     sent = replace_subject_code(sent)
-    doc = expand_nlp(sent)
+
+    doc = nlp(sent)
     print(doc)
+
     displacy.serve(doc, style="dep", port=9353)
-
-    # doc = constituency_nlp(doc)
-    # print(doc)
-
-    # doc = structure_nlp(doc)
-    # print(doc)
 
     j = extract_doc(doc)
     return j
--- bianco/requisites/nlp.py
+++ bianco/requisites/nlp.py
@@ -1,6 +1,3 @@
 import spacy
 
-from requisites.pipes.merge_course_number import *
-
 nlp = spacy.load("en_core_web_sm", exclude=["ner"])
-nlp.add_pipe("merge_course_number", before="detect_entity")
