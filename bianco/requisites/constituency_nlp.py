fa1fddfde99bbebbb95673f475332d8ef612d82b
Yunfan Yang <cloudy.young@outlook.com>
2024-05-19 00:12:05 -0600

    Add pipeline

--- bianco/requisites/constituency_nlp.py
+++ bianco/requisites/constituency_nlp.py
@@ -1,3 +1,8 @@
 import spacy
 
 constituency_nlp = spacy.load("en_core_web_sm", exclude=["ner"])
+
+for t in range(1, 2):
+    constituency_nlp.add_pipe("constitute_requisite", f"constitute_requisite_{t}")
+    constituency_nlp.add_pipe("detect_entity", f"detect_entity_{t}")
+    constituency_nlp.add_pipe("merge_entity_spans", f"merge_entity_spans_{t}")
--- bianco/requisites/expand_nlp.py
+++ bianco/requisites/expand_nlp.py
@@ -1,3 +1,8 @@
 import spacy
 
 expand_nlp = spacy.load("en_core_web_sm", exclude=["ner"])
+expand_nlp.add_pipe("merge_course_number", after="parser")
+expand_nlp.add_pipe("fix_ent_head")
+expand_nlp.add_pipe("expand_course_code")
+expand_nlp.add_pipe("detect_entity")
+expand_nlp.add_pipe("merge_entity_spans")
