e26c659f2e7a7cafacf0c8cdd1162cb69ca547d0
Yunfan Yang <cloudy.young@outlook.com>
2024-05-19 01:28:07 -0600

    Resolve detect entity

--- bianco/requisites/methods.py
+++ bianco/requisites/methods.py
@@ -5,5 +5,5 @@ from requisites.utils import extract_doc, replace_subject_code
 def try_nlp(course: dict, sent: str):
     sent = replace_subject_code(sent)
     doc = nlp(sent)
-    j = extract_doc(doc)
-    return j
+    json_logic = extract_doc(doc)
+    return doc, json_logic
--- bianco/requisites/pipes/detect_entity.py
+++ bianco/requisites/pipes/detect_entity.py
@@ -1,3 +1,6 @@
+from spacy.language import Language
+from spacy.pipeline import EntityRuler
+
 patterns = [
     {
         "label": "COURSE",
@@ -11,3 +14,18 @@ patterns = [
         ],
     },
 ]
+
+
+@Language.factory("detect_entity")
+def create_detect_entity(nlp: Language, name: str):
+    entity_ruler: EntityRuler = nlp.add_pipe(
+        "entity_ruler", config={"overwrite_ents": True}
+    )
+    entity_ruler.add_patterns(patterns)
+
+    def detect_entity(doc):
+        ents = entity_ruler.match(doc)
+        doc.ents = ents
+        return doc
+
+    return detect_entity
