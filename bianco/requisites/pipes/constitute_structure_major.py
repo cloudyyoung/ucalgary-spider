bc4cd97e0ba7567f084ed4fe5c1a334d847bf1dd
Yunfan Yang <cloudy.young@outlook.com>
2024-05-20 02:16:37 -0600

    Refactoring

--- bianco/requisites/pipes/constitute_structure_major.py
+++ bianco/requisites/pipes/constitute_structure_major.py
@@ -2,78 +2,22 @@ from spacy.language import Language
 from spacy.tokens import Doc
 from spacy.matcher import Matcher
 
-from bianco.requisites.utils import (
-    get_dynamic_patterns,
-    replacement_letters,
-    copy_span,
-)
+from bianco.requisites.utils import replacement_letters, copy_span
 from bianco.requisites.pipes.constitute_requisite import sort_matches
-from bianco.requisites.pipes.constitute_structure_minor import (
-    ands_minor,
-    or_list,
-    match_key,
-)
-
-
-### A; and B; and C; ... and D
-major_and_list_patterns_1 = get_dynamic_patterns(
-    [],
-    [
-        {"ENT_TYPE": {"IN": ["COURSE", "REQUISITE"]}},
-        {"TEXT": {"IN": ["and", ";"]}, "OP": "+"},
-    ],
-    range(1, 20),
-    [
-        {"ENT_TYPE": {"IN": ["COURSE", "REQUISITE"]}},
-    ],
-)
-major_and_list_patterns_2 = get_dynamic_patterns(
-    [{"ENT_TYPE": {"IN": ["COURSE", "REQUISITE"]}}],
-    [
-        {"TEXT": {"IN": [",", ";"]}, "OP": "?"},
-        {"ENT_TYPE": {"IN": ["COURSE", "REQUISITE"]}},
-    ],
-    range(1, 20),
-    [],
-)
-### A; and B; and C; ... and D
-
-
-### A; or B; or C; ... or D
-major_or_list_patterns = get_dynamic_patterns(
-    [],
-    [
-        {"ENT_TYPE": {"IN": ["COURSE", "REQUISITE"]}},
-        {"TEXT": {"IN": ["or", ";"]}, "OP": "+"},
-    ],
-    range(1, 20),
-    [
-        {"ENT_TYPE": {"IN": ["COURSE", "REQUISITE"]}},
-    ],
-)
-### A; or B; or C; ... or D
+from bianco.requisites.pipes.patterns.ands_major import ands_major, ands_major_patterns
+from bianco.requisites.pipes.patterns.ors_major import ors_minor, ors_major_patterns
 
 
 @Language.factory("constitute_structure_major")
 def constitute_structure_major(nlp: Language, name: str):
     matcher = Matcher(nlp.vocab)
-    matcher.add(
-        "A; and B; and C; ... and D",
-        major_and_list_patterns_1 + major_and_list_patterns_2,
-        greedy="LONGEST",
-        on_match=ands_minor,
-    )
-    matcher.add(
-        "A; or B; or C; ... or D",
-        major_or_list_patterns,
-        greedy="LONGEST",
-        on_match=or_list,
-    )
+    matcher.add(1, ors_major_patterns, greedy="LONGEST", on_match=ors_minor)
+    matcher.add(2, ands_major_patterns, greedy="LONGEST", on_match=ands_major)
 
     def constitute(doc: Doc):
         while matches := matcher(doc):
             # sort matches by length of span
-            matches = sort_matches(matches, match_key)
+            matches = sort_matches(matches)
             match_id, start, end = matches[0]
 
             letter = next(replacement_letters)
---
+++ bianco/requisites/pipes/patterns/ors_major.py
@@ -0,0 +1,22 @@
+from spacy.tokens import Doc
+from spacy.matcher import Matcher
+
+from bianco.requisites.utils import get_dynamic_patterns
+from bianco.requisites.pipes.patterns.ors_minor import ors_minor
+
+
+def ors_major(matcher: Matcher, doc: Doc, i, matches):
+    return ors_minor(matcher, doc, i, matches)
+
+
+ors_major_patterns = major_or_list_patterns = get_dynamic_patterns(
+    [],
+    [
+        {"ENT_TYPE": {"IN": ["COURSE", "REQUISITE"]}},
+        {"TEXT": {"IN": ["or", ";"]}, "OP": "+"},
+    ],
+    range(1, 20),
+    [
+        {"ENT_TYPE": {"IN": ["COURSE", "REQUISITE"]}},
+    ],
+)
