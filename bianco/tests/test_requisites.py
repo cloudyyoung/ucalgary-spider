bb8477ea8bfd687d22456d4d18d0406c4b4eb184
Yunfan Yang <cloudy.young@outlook.com>
2024-05-19 19:26:02 -0600

    Update test_requisites.py

--- bianco/tests/test_requisites.py
+++ bianco/tests/test_requisites.py
@@ -53,17 +53,33 @@ from bianco.requisites.methods import try_nlp
 
 def test_1():
     sent = "Actuarial Science 327; Statistics 323; 3 units from Mathematics 311, 313, 367 or 375; and 3 units from Computer Science 217, 231, 235 or Data Science 211."
-    doc, json = try_nlp({}, sent)
+    doc, json_logic = try_nlp({}, sent)
 
-    assert json == {
+    assert json_logic == {
         "and": [
-            {"and": [{"course": "GLGY201"}, {"course": "GLGY202"}]},
-            {"or": [{"course": "MATH267"}, {"course": "MATH277"}]},
+            {"course": "ACSC327"},
+            {"course": "STAT323"},
             {
-                "and": [
-                    {"or": [{"course": "PHYS211"}, {"course": "PHYS221"}]},
-                    {"course": "PHYS223"},
-                ]
+                "units": {
+                    "from": [
+                        {"course": "MATH 311"},
+                        {"course": "MATH 313"},
+                        {"course": "MATH 367"},
+                        {"course": "MATH 375"},
+                    ],
+                    "required": 3,
+                }
+            },
+            {
+                "units": {
+                    "from": [
+                        {"course": "CPSC 217"},
+                        {"course": "CPSC 231"},
+                        {"course": "CPSC 235"},
+                        {"course": "DATA 211"},
+                    ],
+                    "required": 3,
+                }
             },
         ]
     }
