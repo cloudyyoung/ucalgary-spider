c0ce955f8ad8cafec7a8faae432f808472465c26
Yunfan Yang <cloudy.young@outlook.com>
2024-05-20 00:17:53 -0600

    Update main.py

--- bianco/main.py
+++ bianco/main.py
@@ -14,29 +14,28 @@ courses_prereq.delete_many({})
 courses_antireq = catalog.get_collection("courses_antireq")
 courses_antireq.delete_many({})
 
+courses_coreq = catalog.get_collection("courses_coreq")
+courses_coreq.delete_many({})
+
 for course in courses:
     prereq = course["prereq"]
     antireq = course["antireq"]
+    coreq = course["coreq"]
 
     if prereq:
-        print(prereq)
-
         doc, json_logic = try_nlp(course, prereq)
-
-        print(json_logic)
-        print("")
-
         courses_prereq.insert_one(
             {"course": course["code"], "prereq_text": prereq, "prereq": json_logic}
         )
 
     if antireq:
-        print(antireq)
         doc, json_logic = try_nlp(course, antireq, mode="antireq")
-
-        print(json_logic)
-        print("")
-
         courses_antireq.insert_one(
             {"course": course["code"], "antireq_text": antireq, "antireq": json_logic}
+        )
+
+    if coreq:
+        doc, json_logic = try_nlp(course, coreq)
+        courses_coreq.insert_one(
+            {"course": course["code"], "coreq_text": coreq, "coreq": json_logic}
         )
