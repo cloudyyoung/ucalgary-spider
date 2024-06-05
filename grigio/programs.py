f510d3c655c9f0acb4a5fa97ff25815b1fc61447
Yunfan Yang <cloudy.young@outlook.com>
2024-06-05 00:56:56 -0600

    Create programs.py

---
+++ grigio/programs.py
@@ -0,0 +1,21 @@
+from tqdm import tqdm
+
+from grigio.utils import DIRTY_CATALOG_DB, CATALOG_DB, process_requisites
+
+programs = list(DIRTY_CATALOG_DB.get_collection("programs").find({}))
+
+
+# Process all course sets
+programs_catalog = CATALOG_DB.get_collection("programs")
+programs_catalog.delete_many({})
+
+
+for programs in tqdm(programs, desc="Programs"):
+    programs = programs.copy()
+
+    # Process requisites
+    requisites = programs["requisites"]
+    programs["requisites"] = process_requisites(requisites)
+
+    # Insert course set
+    programs_catalog.insert_one(programs)
