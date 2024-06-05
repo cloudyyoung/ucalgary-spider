1b6281c57800f48753990f87858b6695a2031ade
Yunfan Yang <cloudy.young@outlook.com>
2024-06-04 23:30:28 -0600

    Update courses.py

--- grigio/courses.py
+++ grigio/courses.py
@@ -16,7 +16,7 @@ courses_catalog = CATALOG_DB.get_collection("courses")
 courses_catalog.delete_many({})
 
 
-for dirty_course in tqdm(dirty_courses):
+for dirty_course in tqdm(dirty_courses, title="Courses"):
     course = dirty_course.copy()
 
     # Process prereq, antireq, coreq
