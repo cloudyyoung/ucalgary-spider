875c03cd1f4333a377b9894afdb41f12a60f912a
Yunfan Yang <cloudy.young@outlook.com>
2024-11-24 01:13:36 -0700

    Update courses.py

--- gpt/courses.py
+++ gpt/courses.py
@@ -1,5 +1,12 @@
 from gpt.utils import courses_collection, openai_client
 
+any_of = [
+    {"$ref": "#/$defs/and"},
+    {"$ref": "#/$defs/or"},
+    {"$ref": "#/$defs/units"},
+    {"type": "string"},
+]
+
 response_format = {
     "type": "json_schema",
     "json_schema": {
@@ -9,14 +16,7 @@ response_format = {
             "type": "object",
             "required": ["requisite"],
             "additionalProperties": False,
-            "properties": {
-                "requisite": {
-                    "anyOf": [
-                        {"$ref": "#/$defs/and"},
-                        {"$ref": "#/$defs/or"},
-                    ]
-                }
-            },
+            "properties": {"requisite": {"anyOf": any_of}},
             "$defs": {
                 "and": {
                     "type": "object",
@@ -27,13 +27,7 @@ response_format = {
                         "and": {
                             "type": "array",
                             "description": "and",
-                            "items": {
-                                "anyOf": [
-                                    {"$ref": "#/$defs/and"},
-                                    {"$ref": "#/$defs/or"},
-                                    {"type": "string"},
-                                ]
-                            },
+                            "items": {"anyOf": any_of},
                             "additionalProperties": False,
                         }
                     },
@@ -47,14 +41,40 @@ response_format = {
                         "or": {
                             "type": "array",
                             "description": "or",
-                            "items": {
-                                "anyOf": [
-                                    {"$ref": "#/$defs/and"},
-                                    {"$ref": "#/$defs/or"},
-                                    {"type": "string"},
-                                ]
-                            },
+                            "items": {"anyOf": any_of},
                             "additionalProperties": False,
+                        }
+                    },
+                },
+                "units": {
+                    "type": "object",
+                    "description": "X units",
+                    "required": ["units"],
+                    "additionalProperties": False,
+                    "properties": {
+                        "units": {
+                            "type": "object",
+                            "description": "units",
+                            "required": ["number", "from", "include"],
+                            "additionalProperties": False,
+                            "properties": {
+                                "number": {
+                                    "type": "integer",
+                                    "description": "number of units",
+                                },
+                                "from": {
+                                    "type": "array",
+                                    "description": "from a list of objects",
+                                    "items": {"anyOf": any_of},
+                                    "additionalProperties": False,
+                                },
+                                "include": {
+                                    "type": "array",
+                                    "description": "include a list of objects",
+                                    "items": {"anyOf": any_of},
+                                    "additionalProperties": False,
+                                },
+                            },
                         }
                     },
                 },
@@ -77,11 +97,11 @@ completion = openai_client.chat.completions.create(
         },
         {
             "role": "user",
-            "content": "Engineering 201 or 212; and Mathematics 275 or Applied Mathematics 217.",
+            "content": "Digital Engineering 319 and 3 units from Sustainable Systems Engineering 315, Engineering 209 or Economic 209.",
         },
     ],
-    response_format=response_format, # type: ignore
+    response_format=response_format,  # type: ignore
 )
 
-research_paper = completion.choices[0].message
+research_paper = completion.choices[0].message.content
 print(research_paper)
