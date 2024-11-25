aed2e6f8fb658736ed4bfe21410612c16b904a7a
Yunfan Yang <cloudy.young@outlook.com>
2024-11-24 20:44:35 -0700

    Remove old pipeline

--- grigio/main.py
+++
@@ -1,7 +0,0 @@
-import grigio.courses
-import grigio.departments
-import grigio.faculties
-import grigio.subject_codes
-import grigio.course_sets
-import grigio.requisite_sets
-import grigio.programs
